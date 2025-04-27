ac5a6338f38a67d28d290821f831b2b83cd9169d
darkGit22 <darkdill22@gmail.com>
2025-04-27 17:27:35 -0400

    Create headerQuantum.h

---
+++ headerQuantum.h
@@ -0,0 +1,10 @@
+#ifndef ADIL_QUANTUM_H
+#define ADIL_QUANTUM_H
+
+#include <fstream>     /
+#include <bitset>      // For bit manipulation
+#include <iostream>    
+#include <string>     
+#include "math.h"
+
+#endif
