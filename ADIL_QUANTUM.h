6085d0d1ce86f8da56c6214bf8f3d1a63aedc202
darkGit22 <darkdill22@gmail.com>
2025-04-29 19:40:57 -0400

    Update ADIL_QUANTUM.h

--- ADIL_QUANTUM.h
+++ ADIL_QUANTUM.h
@@ -1,10 +1,10 @@
 #ifndef ADIL_QUANTUM_H
 #define ADIL_QUANTUM_H
 
-#include <fstream>     /
-#include <bitset>      // For bit manipulation
-#include <iostream>    
-#include <string>     
-#include "math.h"
+#include <fstream>     // for file hndling
+#include <bitset>      // for bit conversions
+#include <iostream>
+#include <string>      //  string handling
+#include <cstdlib>     // for rand
 
-#endif
+#endif /* ADIL_QUANTUM_H */
